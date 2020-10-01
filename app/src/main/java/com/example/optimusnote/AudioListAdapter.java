57a5513888df09e05e4ca7be06ace3231ea30755
Dinuja-Spinto <sri.prabasha@gmail.com>
2020-10-01 10:16:26 +0530

    Morning commit

--- app/src/main/java/com/example/optimusnote/AudioListAdapter.java
+++ app/src/main/java/com/example/optimusnote/AudioListAdapter.java
@@ -1,25 +1,48 @@
 package com.example.optimusnote;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.preference.PreferenceFragment;
 import android.view.LayoutInflater;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.PopupMenu;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.snackbar.Snackbar;
 
 import org.w3c.dom.Text;
 
 import java.io.File;
+import java.util.ArrayList;
 
-public class AudioListAdapter extends RecyclerView.Adapter<AudioListAdapter.AudioViewHolder> {
+import static java.security.AccessController.getContext;
 
-    private File [] allFiles;
+public class AudioListAdapter extends RecyclerView.Adapter<AudioListAdapter.AudioViewHolder> /*implements View.OnClickListener*/ {
+
+    private File[] allFiles;
     private TimeAgo timeAgo;
     private onItemListClick onItemListClick;
+    //private Context rContext;
+    private OnItemOptionListener onItemOptionListener = null;
 
+    int i;
     AudioListAdapter(File[] allFiles,onItemListClick onItemListClick){
         this.allFiles = allFiles;
         this.onItemListClick = onItemListClick;
@@ -35,30 +58,137 @@ public class AudioListAdapter extends RecyclerView.Adapter<AudioListAdapter.Audi
     }
 
     @Override
-    public void onBindViewHolder(@NonNull AudioViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull AudioViewHolder holder, final int position) {
         holder.list_title.setText(allFiles[position].getName());
         holder.list_date.setText(timeAgo.getTimeAgo(allFiles[position].lastModified()));
+        //menu
+
+        holder.btnMore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                showMenu(v,position);
+
+            }
+        });
+
+        /*holder.btnMore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AppCompatActivity activity =(AppCompatActivity) view.getContext();
+                Fragment fragment = new AudiolistFragment();
+
+                activity.getSupportFragmentManager().beginTransaction().replace(R.id.audiolistFragment,fragment).addToBackStack(null).commit();
+            }
+        });*/
 
 
     }
+
+
+
+    public interface OnItemOptionListener {
+        void onItemOptionSelected(int menuId, File item);
+    }
+
+
+
+
+
+    private void showMenu(View v, final int pos) {
+        PopupMenu popup = new PopupMenu(v.getContext(), v);
+        popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                if (onItemOptionListener != null && allFiles.length > pos) {
+                    onItemOptionListener.onItemOptionSelected(item.getItemId(), allFiles[pos]);
+                }
+                return false;
+            }
+        });
+        MenuInflater inflater = popup.getMenuInflater();
+        inflater.inflate(R.menu.file_menu, popup.getMenu());
+
+        popup.show();
+
+        popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+
+                switch (item.getItemId()) {
+
+                    case R.id.menu_delete:
+                        allFiles[pos].delete();
+                        notifyItemRemoved(pos);
+                        notifyItemRangeChanged(pos,allFiles.length);
+                        //Fragment listFragment = new AudiolistFragment();
+
+
+                        break;
+
+                }
+                return false;
+            }
+        });
+
+    }
+
+    public void setOnItemListClick(AudioListAdapter.onItemListClick onItemListClick) {
+        this.onItemListClick = onItemListClick;
+    }
+
+    /*public void refresh(View view){
+        AppCompatActivity activity =(AppCompatActivity) view.getContext();
+        Fragment fragment = new AudiolistFragment();
+
+        activity.getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,fragment).addToBackStack(null).commit();
+    }*/
+
+    //public void onClick(View v) {
+        /*switch (v.getId()) {
+            case R.id.menu_delete:
+
+
+                break;
+        }*/
+
+   // }
+
+
+
+    void setOnItemOptionListener(OnItemOptionListener onItemOptionListener) {
+        this.onItemOptionListener = onItemOptionListener;
+    }
+
+
+
+    //Menu with delete
+
+
 
     @Override
     public int getItemCount() {
         return allFiles.length;
     }
 
+    /*public interface OnItemOptionListener {
+        void onItemOptionSelected(int menuId, File item);
+    }*/
+
     public class AudioViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
         private ImageView list_image;
         private TextView list_title;
         private TextView list_date;
+        private ImageButton btnMore;
+
 
         public AudioViewHolder(@NonNull View itemView) {
             super(itemView);
             list_image = itemView.findViewById(R.id.list_image_view);
             list_title = itemView.findViewById(R.id.list_title);
             list_date = itemView.findViewById(R.id.list_date);
-
+            btnMore = itemView.findViewById(R.id.list_item_more);
+            //btndel = itemView.findViewById(R.id.menu_delete);
             itemView.setOnClickListener(this);
         }
 
@@ -66,9 +196,18 @@ public class AudioListAdapter extends RecyclerView.Adapter<AudioListAdapter.Audi
         public void onClick(View view) {
             onItemListClick.onClickListner(allFiles[getAdapterPosition()],getAdapterPosition());
         }
+
+
     }
+
+    public interface clickOn{
+        void click(View v,int position);
+    }
+
+
 
     public interface onItemListClick{
         void onClickListner(File file,int position);
+
     }
 }
--- app/src/main/java/com/example/optimusnote/AudiolistFragment.java
+++ app/src/main/java/com/example/optimusnote/AudiolistFragment.java
@@ -1,5 +1,7 @@
 package com.example.optimusnote;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.media.MediaPlayer;
 import android.os.Build;
 import android.os.Bundle;
@@ -9,20 +11,28 @@ import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Handler;
 import android.util.Log;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.PopupMenu;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
+import com.google.android.material.snackbar.Snackbar;
 
 import java.io.File;
 import java.io.IOException;
@@ -32,7 +42,7 @@ import java.io.IOException;
  * Use the {@link AudiolistFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class AudiolistFragment extends Fragment implements AudioListAdapter.onItemListClick{
+public class AudiolistFragment extends Fragment implements AudioListAdapter.onItemListClick {
 
     private ConstraintLayout playerSheet;
     private BottomSheetBehavior bottomSheetBehavior;
@@ -47,6 +57,10 @@ public class AudiolistFragment extends Fragment implements AudioListAdapter.onIt
     private File fileToPlay = null;
 
     //UI Elements
+    private ImageButton moreBtn;
+
+    private AudioListAdapter.OnItemOptionListener onItemOptionListener = null;
+
     private ImageButton playBtn;
     private TextView playerHeader;
     private TextView playerFileName;
@@ -54,6 +68,8 @@ public class AudiolistFragment extends Fragment implements AudioListAdapter.onIt
     private SeekBar playerSeekB;
     private Handler seekBarHandler;
     private Runnable updateSeek;
+
+    private Context rContext;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -63,6 +79,8 @@ public class AudiolistFragment extends Fragment implements AudioListAdapter.onIt
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    //for refresh
+    int count=0;
 
     public AudiolistFragment() {
         // Required empty public constructor
@@ -93,7 +111,11 @@ public class AudiolistFragment extends Fragment implements AudioListAdapter.onIt
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+        //refresh
+        //content();
     }
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -110,6 +132,7 @@ public class AudiolistFragment extends Fragment implements AudioListAdapter.onIt
         bottomSheetBehavior = BottomSheetBehavior.from(playerSheet);
         audioList = view.findViewById(R.id.rec_list_view);
 
+        moreBtn = view.findViewById(R.id.list_item_more);
         playBtn = view.findViewById(R.id.play_btn);
         playerHeader = view.findViewById(R.id.player_header_title);
         playerFileName = view.findViewById(R.id.file_name);
@@ -125,6 +148,23 @@ public class AudiolistFragment extends Fragment implements AudioListAdapter.onIt
         audioList.setLayoutManager(new LinearLayoutManager(getContext()));
         audioList.setAdapter(audioListAdapter);
 
+        /*moreBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        moreBtn.setOnCreateContextMenuListener(new View.OnCreateContextMenuListener() {
+            @Override
+            public void onCreateContextMenu(ContextMenu contextMenu, View view, ContextMenu.ContextMenuInfo contextMenuInfo) {
+
+
+                showMenu(view,position);
+            }
+        });*/
+
+
 
         bottomSheetBehavior.addBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
             @Override
@@ -139,6 +179,9 @@ public class AudiolistFragment extends Fragment implements AudioListAdapter.onIt
 
             }
         });
+
+
+
         playBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -175,8 +218,93 @@ public class AudiolistFragment extends Fragment implements AudioListAdapter.onIt
         });
     }
 
+    public void delRec(int position){
+
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.file_menu,menu);
+
+
+    }
+
+
+
+    /*audioList.setOnItemOptionListener(new AudioListAdapter.OnItemOptionListener() {
+        @Override
+        public void onItemOptionSelected(int menuId, File[] files) {
+            switch (menuId) {
+                case R.id.menu_rename:
+                    //presenter.onShareRecordClick();
+                    break;
+
+                case R.id.menu_delete:
+                    allFiles[pos].delete();
+                    //Fragment fragment = new Fragment(R.layout.player_sheet);
+                    // Intent intent = new Intent(,AudiolistFragment);
+                    //PreferenceFragment.OnPreferenceStartFragmentCallback.class.getClass();
+                    //fragment.onStart();
+                    //fragment.startActivity();
+                    //new AudiolistFragment().onStart();
+
+
+                    //notifyItemRemoved(pos);
+
+                    break;
+
+
+            }
+        }
+    })*/
+
+
+
+
+    /*audioListAdapter.setOnItemListClick(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                /*if (onItemOptionListener != null && getItemCount() > pos) {
+                    onItemOptionListener.onItemOptionSelected(item.getItemId(), allFiles[pos]);
+                }
+                switch (item.getItemId()) {
+
+                    case R.id.menu_delete:
+                        allFiles[pos].delete();
+                        //Fragment fragment = new Fragment(R.layout.player_sheet);
+                       // Intent intent = new Intent(,AudiolistFragment);
+                        //PreferenceFragment.OnPreferenceStartFragmentCallback.class.getClass();
+                        //fragment.onStart();
+                        //fragment.startActivity();
+                        //new AudiolistFragment().onStart();
+
+
+                        //notifyItemRemoved(pos);
+
+                        break;
+
+                }
+                return false;
+            }
+        });*/
+
+
+
+
+
+    /*private void deleteFile(File fileToDel, int position){
+
+        allFiles[position].delete();
+        audioListAdapter.notifyItemRemoved();
+        audioListAdapter.notifyItemRangeChanged();
+        Snackbar.make(v,"File Deleted",Snackbar.LENGTH_SHORT).show();
+    }*/
+
+
     @Override
     public void onClickListner(File file, int position) {
+
         //Log.d("PLAY_LOG","file_playing"+file.getName());
         if(isPlaying){
             stopAudio();
@@ -189,7 +317,43 @@ public class AudiolistFragment extends Fragment implements AudioListAdapter.onIt
             playAudio(fileToPlay);
 
         }
+
+        if(getView().getId() == R.id.menu_delete){
+            allFiles[position].delete();
+        }
+
+
+
     }
+
+    /*public void click(View v,int position){
+
+            switch(getId()){
+                case R.id.menu_delete:
+                    allFiles[position].delete();
+                    break;
+            }
+
+    }*/
+
+    //refresh
+    /*public void content(){
+        count++;
+
+        refresh(1000);
+    }
+    //refresh
+    private void refresh(int milliseconds) {
+        final Handler handler= new Handler();
+        final Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                content();
+            }
+        };
+
+        handler.postDelayed(runnable,milliseconds);
+    }*/
 
     private void pauseAudio(){
         mediaPlayer.pause();
