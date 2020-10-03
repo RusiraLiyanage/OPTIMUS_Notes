702acf184e5ae596eb607065aed9d5073996203f
iresha711 <uisandamali7711@gmail.com>
2020-10-03 22:55:10 +0530

    delete item test cases

--- app/src/test/java/com/example/optimusnote/ExampleUnitTest.java
+++ app/src/test/java/com/example/optimusnote/ExampleUnitTest.java
@@ -1,5 +1,8 @@
 package com.example.optimusnote;
 
+import com.example.optimusnote.activities.Mainchecklist;
+
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -10,8 +13,17 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+    private Mainchecklist mainchecklist;
+
+
+    @Before
+    public void setup(){
+        mainchecklist = new Mainchecklist();
+    }
     @Test
+
     public void addition_isCorrect() {
+
         assertEquals(4, 2 + 2);
     }
 }
